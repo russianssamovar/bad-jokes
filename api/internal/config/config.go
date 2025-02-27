05ec3d1fa3d09a30bee4ea342bb4f6fe9005d992
a.alekseev <a.alekseev@pnlfin.tech>
2025-02-27 13:42:31 +0200

    first

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -10,21 +10,21 @@ import (
 )
 
 type Config struct {
-	Env        string         `yaml:"env" env-default:"local" env-required:"true"`
+	Env        string         `yaml:"env" env:"ENV" env-default:"local" env-required:"true"`
 	Db         DatabaseConfig `yaml:"db"`
 	HTTPServer HTTPServer     `yaml:"http_server"`
-	JWTSecret  string         `yaml:"jwt_secret" env-required:"true"`
+	JWTSecret  string         `yaml:"jwt_secret" env:"JWT_SECRET" env-required:"true"`
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080"`
-	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
-	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	Address     string        `yaml:"address" env:"HTTP_SERVER_ADDRESS" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env:"HTTP_SERVER_TIMEOUT" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env:"HTTP_SERVER_IDLE_TIMEOUT" env-default:"60s"`
 }
 
 type DatabaseConfig struct {
-	ConnectionString string `yaml:"connection_string" env-required:"true"`
-	Driver           string `yaml:"driver" env-required:"true"`
+	ConnectionString string `yaml:"connection_string" env:"DB_CONNECTION_STRING" env-required:"true"`
+	Driver           string `yaml:"driver" env:"DB_DRIVER" env-required:"true"`
 }
 
 func MustLoad() *Config {
